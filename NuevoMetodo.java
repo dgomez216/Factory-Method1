6b934245073ce02bffce515f0b537ed668bbaa4b
dgomez216 <66398613+dgomez216@users.noreply.github.com>
2020-06-15 20:30:52 -0500

    Create NuevoMetodo.java

---
+++ NuevoMetodo.java
@@ -0,0 +1,7 @@
+public class ContadorAscendenteIngles implements Contador {
+    @Override
+    public String cuenta() {
+        return "One, two, three, four, five,
+            six, seven, eight, nine and ten";
+    }
+}
